b16fa5677e09137cc5b0cd74b6bc6ee61d263bfd
Levi Kane <oneleggedlevi09@gmail.com>
2021-05-06 20:42:20 -0500

    models index.js relationship has now been set up

--- models/index.js
+++ models/index.js
@@ -11,26 +11,13 @@ Category.hasMany(Product, {
   foreignKey: 'category_id',
   onDelete: 'CASCADE'
 });
+Product.belongsToMany(Tag, {
+  through: ProductTag
+});
+Tag.belongsToMany(Product, {
+  through: ProductTag
+});
 
-// Driver.hasOne(License, {
-//     foreignKey: 'driver_id',
-//     onDelete: 'CASCADE',
-//   });
-
-//   License.belongsTo(Driver, {
-//     foreignKey: 'driver_id',
-//   });
-
-//   // Define a Driver as having many Cars, thus creating a foreign key in the `car` table
-//   Driver.hasMany(Car, {
-//     foreignKey: 'driver_id',
-//     onDelete: 'CASCADE',
-//   });
-
-//   // The association can also be created from the Car side
-//   Car.belongsTo(Driver, {
-//     foreignKey: 'driver_id',
-//   });
 // Products belongsTo Category
 
 // Categories have many Products

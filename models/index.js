aaf1cdbdd87ae526cede3dd5549f8eaa281f6fa2
Levi Kane <oneleggedlevi09@gmail.com>
2021-05-06 20:33:44 -0500

    added the product and category relationship in the index.js file

--- models/index.js
+++ models/index.js
@@ -4,6 +4,33 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+});
+
+// Driver.hasOne(License, {
+//     foreignKey: 'driver_id',
+//     onDelete: 'CASCADE',
+//   });
+
+//   License.belongsTo(Driver, {
+//     foreignKey: 'driver_id',
+//   });
+
+//   // Define a Driver as having many Cars, thus creating a foreign key in the `car` table
+//   Driver.hasMany(Car, {
+//     foreignKey: 'driver_id',
+//     onDelete: 'CASCADE',
+//   });
+
+//   // The association can also be created from the Car side
+//   Car.belongsTo(Driver, {
+//     foreignKey: 'driver_id',
+//   });
 // Products belongsTo Category
 
 // Categories have many Products
@@ -16,5 +43,5 @@ module.exports = {
   Product,
   Category,
   Tag,
-  ProductTag,
+  ProductTag
 };

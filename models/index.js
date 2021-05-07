7efd9ac0611d01f8777b25a68d0aa420212d5256
Levi Kane <oneleggedlevi09@gmail.com>
2021-05-06 21:21:27 -0500

    deleted some unecessary code comments

--- models/index.js
+++ models/index.js
@@ -18,14 +18,6 @@ Tag.belongsToMany(Product, {
   through: ProductTag
 });
 
-// Products belongsTo Category
-
-// Categories have many Products
-
-// Products belongToMany Tags (through ProductTag)
-
-// Tags belongToMany Products (through ProductTag)
-
 module.exports = {
   Product,
   Category,
